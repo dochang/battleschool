6597d6330383eb0f61a3ae34b715a64248cc7310
Desmond O. Chang <dochang@gmail.com>
2015-05-30 12:32:29 +0800

    Set `DIST_MODULE_PATH` from `sys.prefix` even if outside a virtualenv.
    People may install python into a non-standard location other than
    `/usr`, such as `$HOME`.  That means `DIST_MODULE_PATH` should be always
    based on `sys.prefix`.

--- lib/battleschool/constants.py
+++ lib/battleschool/constants.py
@@ -11,12 +11,7 @@ def get_config(env_var, default):
     return default
 
 
-# Needed so the RPM can call setup.py and have modules land in the
-# correct location. See #1277 for discussion
-if getattr(sys, "real_prefix", None):
-    DIST_MODULE_PATH = os.path.join(sys.prefix, 'share/battleschool/')
-else:
-    DIST_MODULE_PATH = '/usr/share/battleschool/'
+DIST_MODULE_PATH = os.path.join(sys.prefix, 'share/battleschool/')
 
 DEFAULT_MODULE_PATH = get_config('BATTLESCHOOL_LIBRARY', os.path.join(DIST_MODULE_PATH, 'library'))
 DEFAULT_HOST_LIST = os.path.expanduser(get_config('BATTLESCHOOL_HOSTS', os.path.join(DIST_MODULE_PATH, 'defaults', 'hosts')))
